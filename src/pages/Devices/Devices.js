1a92e862b4a55c7c1b02912af2f9939b72ecc5ab
mbajraktar1 <mbajraktar1@etf.unsa.ba>
2021-03-19 14:33:21 +0100

    test access

--- src/pages/Devices/Devices.js
+++ src/pages/Devices/Devices.js
@@ -2,7 +2,7 @@ import { connect } from "react-redux";
 
 const Devices = () => (
     <div className='page devices'>
-        <h1>Lista mašina</h1>
+        <h1> Lista mašina </h1>
     </div>
 );
 
