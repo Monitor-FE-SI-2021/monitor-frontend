e5bdfd3bc01cf5dff1603273135f7a02b40a08fb
Adna <ahalilovi15@etf.unsa.ba>
2021-03-19 16:07:43 +0100

    dodana lista aktivnih masina

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,26 @@
+import React from "react";
 import { connect } from "react-redux";
+import ActiveMachine from "../../components/Layout/components/ActiveMachine";
 
 const Dashboard = () => (
     <div className='page dashboard'>
-        <h1>Dashboard</h1>
+        <h1>List of active machines</h1>
+        <ActiveMachine 
+        img="https://brain-images-ssl.cdn.dixons.com/3/6/10208063/u_10208063.jpg" 
+        name="Machine 1"
+        info="This machine is used for something"
+        />
+        <ActiveMachine 
+        img="https://brain-images-ssl.cdn.dixons.com/3/6/10208063/u_10208063.jpg" 
+        name="Machine 4"
+        info="This machine is used for something"
+        />
+        <ActiveMachine 
+        img="https://brain-images-ssl.cdn.dixons.com/3/6/10208063/u_10208063.jpg" 
+        name="Machine 9"
+        info="This machine is used for something"
+        />
     </div>
 );
 
-export default connect(state => ({}), {})(Dashboard);
+export default connect(state => ({}), {})(Dashboard)
