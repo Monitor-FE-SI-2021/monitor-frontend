13fec7d60231bdae8d0dcdf69e5e9c3bb6169021
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2021-03-20 13:39:44 +0100

    Basic layout of AddDeviceForm

--- src/components/AddDeviceForm/AddDeviceForm.js
+++ src/components/AddDeviceForm/AddDeviceForm.js
@@ -1,9 +1,49 @@
-const AddTaskForm = () => {
-    return (
-        <form>
-            <h3>Dodavanje mašine</h3>
-        </form>
-    )
-}
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
+import MenuItem from '@material-ui/core/MenuItem';
 
-export default AddTaskForm
+const useStyles = makeStyles((theme) => ({
+    root: {
+        '& .MuiFormControl-root': {
+            display: 'flex',
+            flexWrap: 'wrap',
+            width: '50%',
+            margin: theme.spacing(1),
+        }
+    },
+}));
+
+const groups = [
+    {
+        id: 1,
+        name: 'Grupa 1',
+    },
+    {
+        id: 2,
+        name: 'Grupa 2',
+    },
+    {
+        id: 3,
+        name: 'Grupa 3',
+    },
+]
+
+
+export default function AddDeviceForm() {
+    const classes = useStyles();
+
+    return (
+        <form className={classes.root}>
+            <TextField label="Naziv" />
+            <TextField label="Lokacija" />
+            <TextField label="Geografske koordinate" />
+            <TextField select label="Odaberite grupu">
+                {groups.map((group) => (
+                  <MenuItem key={group.id} value={group.name}>
+                    {group.name}
+                  </MenuItem>
+            ))}</TextField>
+        </form>
+    );
+}
--- src/pages/AddDevices/AddDevice.js
+++ src/pages/AddDevices/AddDevice.js
@@ -1,10 +1,24 @@
 import { connect } from "react-redux";
 import AddDeviceForm from "../../components/AddDeviceForm/AddDeviceForm";
+import { Paper,makeStyles } from '@material-ui/core';
 
-const AddDevice = () => (
-    <div className='page devices'>
-        <AddDeviceForm />
-    </div>
-);
+const useStyles = makeStyles(theme => ({
+    pageContent: {
+        margin: theme.spacing(5),
+        padding: theme.spacing(3)
+    }
+}))
+
+const AddDevice = () => {
+    const classes = useStyles();
+
+    return (
+        <div className='page devices'>
+            <Paper className={classes.pageContent}>
+                <AddDeviceForm />
+            </Paper>
+        </div>
+    )
+}
 
 export default connect(state => ({}), {})(AddDevice);
