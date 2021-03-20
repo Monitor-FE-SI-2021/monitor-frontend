842c1924418800b469fb25aeb4b451f924f3d4ed
Ajdin Becirovic <abecirovic3@etf.unsa.ba>
2021-03-20 14:25:29 +0100

    Added static data for group selection, and onClick for button

--- src/components/AddDeviceForm/AddDeviceForm.js
+++ src/components/AddDeviceForm/AddDeviceForm.js
@@ -2,15 +2,22 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import {useState} from 'react'
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        '& .MuiFormControl-root': {
+        '& .MuiFormControl-root, .MuiButton-root': {
             display: 'flex',
             flexWrap: 'wrap',
+            margin: theme.spacing(3),
+        },
+        '& .MuiFormControl-root': {
             width: '50%',
-            margin: theme.spacing(1),
-        }
+        },
+        '.MuiButton-root': {
+            width: '25%',
+        },
     },
 }));
 
@@ -29,21 +36,38 @@ const groups = [
     },
 ]
 
-
 export default function AddDeviceForm() {
     const classes = useStyles();
+
+    const [selectedGroup, setSelectedGroup] = useState('')
+
+    const onClick = () => {
+        console.log('Click')
+    }
+
+    const changeSelectedGroup = (value) => {
+        setSelectedGroup(value)
+    }
 
     return (
         <form className={classes.root}>
             <TextField label="Naziv" />
-            <TextField label="Lokacija" />
-            <TextField label="Geografske koordinate" />
-            <TextField select label="Odaberite grupu">
+            <TextField label="Adresa" />
+            <TextField label="Geografska širina" type='number'/>
+            <TextField label="Geografska visina" type='number'/>
+            <TextField
+                select
+                value={selectedGroup}
+                label="Odaberite grupu"
+                onChange={(e) => (changeSelectedGroup(e.target.value))}
+            >
                 {groups.map((group) => (
                   <MenuItem key={group.id} value={group.name}>
                     {group.name}
                   </MenuItem>
-            ))}</TextField>
+                ))}
+            </TextField>
+            <Button onClick={onClick} variant="contained">Dodaj mašinu</Button>
         </form>
     );
 }
