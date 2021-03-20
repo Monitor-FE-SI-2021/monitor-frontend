a84187059c8ffbf0c97da484ba57175821ebfb1c
ssuljevic <ssuljevic1@etf.unsa.ba>
2021-03-20 21:15:44 +0100

    Feat: loading mock picture from server

--- src/pages/RemoteControl/RemoteControl.js
+++ src/pages/RemoteControl/RemoteControl.js
@@ -1,33 +1,79 @@
 import { connect } from "react-redux";
 import { useState } from "react";
 
-const handleClick = () => {
-    console.log("called");
-    fetch('http://109.237.36.76:25565/screenshot')
-    .then(res => res.text())
-    .then(res => console.log(res));
-}
 
 
-const RemoteControl = () => {
 
-    const [url, setUrl] = useState("slkadhjaksl");
+const RemoteControl =  () => {
+
+    const [url, setUrl] = useState("slkadhjaksl"); 
+   
+    const handleClick = async ()  =>  {
+        
+        
+       const requestOptions = {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ email: 'whoso@whoso.com', password:  'sifra123'})
+        };
+
+        try {
+            var response = await fetch('http://167.99.244.168:3333/login', requestOptions);
+            
+            var x = await response.json();
+            if(x.status == 200)
+            {
+                console.log("all good");
+            }
+            //console.log(x.accessToken);
+
+            const requestOptions2 = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ name: 'whoso@whoso.com', location: 'sifra123'}),
+                headers: {"Authorization" : "Bearer "+ x.accessToken}
+            };
+    
+            var odgovor = await
+            fetch('http://109.237.36.76:25565/screenshot',
+            requestOptions2,
+            );
+            var slika = await odgovor.text();
+            console.log(slika);
+
+            setUrl(slika);
+        }
+        catch(err) {
+
+        }
+
+        
+        
+
+
+        /*const returnValue = fetch('http://167.99.244.168:3333/jwt/verify', {
+        headers: {"Authorization" : "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwiZW1haWwiOiJ3aG9zb0B3aG9zby5jb20iLCJyb2xlSWQiOjEsImdyb3VwSWQiOjIsImlhdCI6MTYxNjI2NzExOCwiZXhwIjoxNjE2MjY4OTE4fQ.XdPonVhLvRTJ2p9stDS35RjSKWcGw-x3Hhs5f3dxwlg"}})
+        .then(res => {  
+            setUrl(res)
+            console.log("ispod je result");
+           console.log(res);
+        });*/
+    }
 
     
-
-
     
+
     return (
         <div className='page dashboard'>
         <h1>IWM Remote Access/Control</h1>
         
         <div>
             <div>
-
+            
             </div>
             <div>
                 <div>
-
+                    <img src={`data:image/jpeg;base64,${url}`} />
                 </div>
             </div>
         </div>
