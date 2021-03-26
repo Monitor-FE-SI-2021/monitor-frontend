9bfa11bfd8e5b5f08c1057e660ab6580010d7844
Enver Suljic <enver13579@windowslive.com>
2021-03-26 16:41:11 +0100

    Remote control navigation

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,137 +1,156 @@
 import { useState } from "react";
-import {useMemo} from "react";
+import { useMemo } from "react";
 import "./Tabs.css";
 import { connect } from "react-redux";
 import request, { wsEndpoint } from "../../service";
-import {useDropzone} from "react-dropzone";
-import React, {useCallback} from "react";
-import Swal from "sweetalert2"
+import { useDropzone } from "react-dropzone";
+import React, { useCallback } from "react";
+import Swal from "sweetalert2";
+import { Route, Switch, useParams } from "react-router";
 
-function Tabs() {
-  const [toggleState, setToggleState] = useState(1);
+const Tabs = (props) => {
+  const machines = ["DESKTOP-SCC", "DESKTOP-SCC3", "DESKTOP-SCC5"];
 
-  const toggleTab = (index) => {
-    setToggleState(index);
+  let { name, tab } = useParams();
+
+  const toggleTab = (tab) => {
+    props.history.push(
+      "/remotecontrol/" + (name == undefined ? machines[0] : name) + "/" + tab
+    );
   };
 
   const [url, setUrl] = useState("slkadhjaksl");
 
   const handleClick = async () => {
     try {
-      var odgovor = await request(wsEndpoint + '/screenshot',
-          "post",
-          { name: 'DESKTOP-SCC', location: 'Sarajevo - SCC' });
+      var odgovor = await request(wsEndpoint + "/screenshot", "post", {
+        name: "DESKTOP-SCC",
+        location: "Sarajevo - SCC",
+      });
 
       setUrl(odgovor.data.message);
-    } catch (err) {
-    }
-  }
-  
-    const baseStyle = {
-      flex: 1,
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'center',
-      textAlign: 'center',
-      padding: '80px',
-      borderWidth: 2,
-      borderRadius: 2,
-      borderColor: '#eeeeee',
-      borderStyle: 'dashed',
-      backgroundColor: '#fafafa',
-      color: '#bdbdbd',
-      outline: 'none',
-      transition: 'border .24s ease-in-out'
-    };
-    
-    const activeStyle = {
-      borderColor: '#2196f3'
-    };
-    
-    const acceptStyle = {
-      borderColor: '#00e676'
-    };
-    
-    const rejectStyle = {
-      borderColor: '#ff1744'
-    };  
+    } catch (err) {}
+  };
 
-  const onDrop = useCallback(acceptedFiles => {
+  const baseStyle = {
+    flex: 1,
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    textAlign: "center",
+    padding: "80px",
+    borderWidth: 2,
+    borderRadius: 2,
+    borderColor: "#eeeeee",
+    borderStyle: "dashed",
+    backgroundColor: "#fafafa",
+    color: "#bdbdbd",
+    outline: "none",
+    transition: "border .24s ease-in-out",
+  };
+
+  const activeStyle = {
+    borderColor: "#2196f3",
+  };
+
+  const acceptStyle = {
+    borderColor: "#00e676",
+  };
+
+  const rejectStyle = {
+    borderColor: "#ff1744",
+  };
+
+  const onDrop = useCallback((acceptedFiles) => {
     //SEND FILES TO SERVER FROM HERE!
     Swal.fire({
       title: "File manager",
       text: "You have successfully uploaded your file!",
-      type: "success"
+      type: "success",
     });
-  }, []);  
+  }, []);
 
   const {
     getRootProps,
     getInputProps,
     isDragActive,
     isDragAccept,
-    isDragReject
-  } = useDropzone({onDrop});
-
-  const style = useMemo(() => ({
-    ...baseStyle,
-    ...(isDragActive ? activeStyle : {}),
-    ...(isDragAccept ? acceptStyle : {}),
-    ...(isDragReject ? rejectStyle : {})
-  }), [
-    isDragActive,
     isDragReject,
-    isDragAccept
-  ]);
+  } = useDropzone({ onDrop });
+
+  const style = useMemo(
+    () => ({
+      ...baseStyle,
+      ...(isDragActive ? activeStyle : {}),
+      ...(isDragAccept ? acceptStyle : {}),
+      ...(isDragReject ? rejectStyle : {}),
+    }),
+    [isDragActive, isDragReject, isDragAccept]
+  );
 
   return (
     <div className="container">
       <div className="bloc-tabs">
         <button
-          className={toggleState === 1 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(1)}
+          className={
+            tab == undefined || tab === "screenshot"
+              ? "tabs active-tabs"
+              : "tabs"
+          }
+          onClick={() => toggleTab("screenshot")}
         >
           Screenshot
         </button>
         <button
-          className={toggleState === 2 ? "tabs active-tabs" : "tabs"}
-          onClick={() => toggleTab(2)}
+          className={tab === "filemanager" ? "tabs active-tabs" : "tabs"}
+          onClick={() => toggleTab("filemanager")}
         >
           File Manager
         </button>
       </div>
       <div className="content-tabs">
         <div
-          className={toggleState === 1 ? "content  active-content" : "content"}
+          className={
+            tab == undefined || tab === "screenshot"
+              ? "content  active-content"
+              : "content"
+          }
         >
           <div>
-                <p className="paragraph1">Quick example</p>
-                <div>
-                    <div className="screenshot">
-                        <p>Screenshot</p>
-                        <img alt="Asked image will appear here."
-                             className='screenshot-img'
-                             src={`data:image/jpeg;base64,${url}`}/>
-                    </div>
-                </div>
+            <p className="paragraph1">Quick example</p>
+            <div>
+              <div className="screenshot">
+                <p>Screenshot</p>
+                <img
+                  alt="Asked image will appear here."
+                  className="screenshot-img"
+                  src={`data:image/jpeg;base64,${url}`}
+                />
+              </div>
             </div>
+          </div>
 
-            <button type="button" onClick={handleClick}>
-                Get Screenshot
-            </button>
+          <button type="button" onClick={handleClick}>
+            Get Screenshot
+          </button>
         </div>
-
-        <div className={toggleState === 2 ? "content  active-content" : "content"}>
+        <div
+          className={
+            tab === "filemanager" ? "content  active-content" : "content"
+          }
+        >
           <div className="uploadArea">
-            <div {...getRootProps({style})}>
+            <div {...getRootProps({ style })}>
               <input {...getInputProps()} />
-              <p className="uploadText">Drag 'n' drop some files here, or click to select files</p>
+              <p className="uploadText">
+                Drag 'n' drop some files here, or click to select files
+              </p>
             </div>
           </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Tabs;
+export default connect((state) => ({}), {})(Tabs);
--- src/pages/RemoteControl/RemoteControl.js
+++ src/pages/RemoteControl/RemoteControl.js
@@ -1,21 +1,39 @@
 import { connect } from "react-redux";
-import { useState } from "react";
-import './RemoteControl.css'
-import request, { wsEndpoint } from "../../service";
-import Tabs from '../../components/Tabs/Tabs'
+import React, { useState } from "react";
+import "./RemoteControl.css";
+import Tabs from "../../components/Tabs/Tabs";
+import "react-dropdown/style.css";
+import { Route, useParams } from "react-router-dom";
 
+const RemoteControl = (props) => {
+  const machines = ["DESKTOP-SCC", "DESKTOP-SCC3", "DESKTOP-SCC5"];
 
-const RemoteControl = () => {
+  let { name, tab } = useParams();
 
-    
-
-    return (
-        <div className='page dashboard'>
-            <h1>IWM Remote Access/Control</h1>
-            <Tabs></Tabs>
-            
-        </div>
+  const switchMachine = (machine) => {
+    props.history.push(
+      "/remotecontrol/" +
+        machine +
+        "/" +
+        (tab == undefined ? "screenshot" : tab)
     );
-}
+  };
 
-export default connect(state => ({}), {})(RemoteControl);
+  return (
+    <div className="page dashboard">
+      <h1>IWM Remote Access/Control</h1>
+      <div>
+        <select
+          onChange={(event) => switchMachine(event.target.value)}
+          value={name}
+        >
+          <option value="DESKTOP-SCC">DESKTOP-SCC</option>
+          <option value="DESKTOP-SCC2">DESKTOP-SCC2</option>
+        </select>
+        <Route component={Tabs}></Route>
+      </div>
+    </div>
+  );
+};
+
+export default connect((state) => ({}), {})(RemoteControl);
