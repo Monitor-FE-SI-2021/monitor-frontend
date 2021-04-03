8697829019eb034548a01d3d353581d2466ff6d2
Mujkanovic Mirza <mmujkanovi1@etf.unsa.ba>
2021-04-04 00:47:00 +0200

    url fix

--- src/components/securityQuestions/Questions.js
+++ src/components/securityQuestions/Questions.js
@@ -8,15 +8,14 @@ import { STORAGE_KEY } from "../../utils/consts";
 import ReactDOM from 'react-dom';
 import { push } from 'connected-react-router';
 import request from "../../service";
+import { showSwalToast } from "../../utils/utils";
 
-
-const securityQuestions = "http://localhost:3333/AllSecurityQuestions";
-const saveQuestions="http://localhost:3333/setSecurityQuestionsAndAnswers/:token"
+const tooken = window.localStorage.getItem(STORAGE_KEY);
+const securityQuestions = "https://si-2021.167.99.244.168.nip.io:3333/AllSecurityQuestions";
+const saveQuestions="https://si-2021.167.99.244.168.nip.io:3333/setSecurityQuestionsAndAnswers";
 
 function Questions() {
     const initialFormData = {
-        email: "",
-        pitanja:[],
         broj : 1
     };
 
@@ -47,63 +46,52 @@ console.log(e.target.value);
   for(let i=0;i<formData.broj+2;i++) {
   const doubleQuestion=FormQuestions.find(element=>element==document.getElementsByClassName("questions")[i].value);
    FormQuestions.push(document.getElementsByClassName("questions")[i].value);
-  var oneQuestion=document.getElementsByClassName("questions")[i].value;
-  const idQuestion=Questions.find(element=>element.Question==oneQuestion);
-  QuestionsID.push(idQuestion.QuestionId);
-
-var json={ "questionId":idQuestion.QuestionId, "answer":document.getElementsByClassName("answers")[i].value };
+  //var oneQuestion=document.getElementsByClassName("questions")[i].value;
+  const DBQuestion=Questions.find(element=>element.Question==document.getElementsByClassName("questions")[i].value);
+ // QuestionsID.push(idQuestion.QuestionId);
+if(DBQuestion) {
+var json={ "questionId":DBQuestion.QuestionId, "answer":document.getElementsByClassName("answers")[i].value };
 if(!doubleQuestion)
 niz.push(json);
   }
-
-
-
-console.log("Pitanjaaaa");
-console.log(FormQuestions);
-console.log("odgovor");
-console.log(FormAnswers);
-console.log("ID");
-console.log(Questions);
-console.log("Jedan Id");
-console.log(Questions[0].QuestionId);
-console.log("idijeeviii");
-    console.log(QuestionsID);
+  }
 console.log("JSON ODGOVORI");
 console.log(niz);
  console.log("TOKEN");
    const token = window.localStorage.getItem(STORAGE_KEY);
  console.log(token);
 
- /*
+
    request(saveQuestions, 'post', niz)
              .then(() => {
-                 QueSuc(true)
+                 setQueSuc(true);
              }).catch((error) => {
              console.log(error);
-             alert('Error');
+              showSwalToast('Invalid answers');
          })
 
-         updateFormData(initialFormData)
+         updateFormData(initialFormData);
+         niz=[];
 
-*/
 
-    };
+    }
 
 
 const AddQuestion=(e)=> {
         e.preventDefault();
         if(formData.broj<=7){
 const myelement = <form><p> Quesstion {formData.broj+3}</p>    <select id="select" className="questions">
-                                                                            {Questions.map(question=>(
-                                                                                          <option key={question.QuestionId}>{question.Question}</option>
-                                                                                          ))}
-                                                                            </select><input type="text" className="answers"/></form>;
+<option value="" selected disabled hidden>Choose here</option>
+   {Questions.map(question=>(
+  <option key={question.QuestionId}>{question.Question}</option>
+           ))}
+   </select><input type="text" className="answers"/></form>;
 var broj=formData.broj;
-var frr="root"+broj;
-const pet=document.getElementById(frr).isEmpty
+var add="root"+formData.broj;
+const pet=document.getElementById(add).isEmpty
 broj++;
-formData.broj=broj;
-ReactDOM.render(myelement,document.getElementById(frr));
+formData.broj++;
+ReactDOM.render(myelement,document.getElementById(add));
 }
 switchRoute('/question');
 };
@@ -127,29 +115,32 @@ useEffect(()=>{
                     <form id="form">
                 <h2>Security questions?</h2>
                 <p>Question 1:</p>
-                <select id="select1" className="questions" onClick={handleChange}>
+                <select id="select1"   className="questions" onClick={handleChange}>
+                <option value="" selected disabled hidden>Choose here</option>
       {Questions.map(question=>(
               <option key={question.QuestionId}>{question.Question}</option>
               ))}
 
                 </select>
-                <input name="email" className="answers" type='email'
+                <input  className="answers" type='text'
                        />
                        <p>Question 2:</p>
                           <select id="select2" className="questions">
+                          <option value="" selected disabled hidden>Choose here</option>
                                        {Questions.map(question=>(
                                                      <option key={question.QuestionId}>{question.Question}</option>
                                                      ))}
                                        </select>
-                                       <input name="vmdfk"className="answers" type='email'
+                                       <input className="answers" type='text'
                                               />
                                               <p>Question 3:</p>
                                                  <select id="select3" className="questions">
+                                                 <option value="" selected disabled hidden>Choose here</option>
                                                               {Questions.map(question=>(
                                                                             <option key={question.QuestionId}>{question.Question}</option>
                                                                             ))}
                                                               </select>
-                                                              <input name="fvddf" className="answers" type='email'
+                                                              <input  className="answers" type='text'
                        />
                <div id="root1"> </div>
                <div id="root2"> </div>
