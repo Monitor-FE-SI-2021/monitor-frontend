d5886e6db4229af3413a87ab9a740b4d58ecbb2f
Irfan Smajevic <smajevic.irfan@gmail.com>
2021-04-05 12:08:53 +0200

    Modular RemoteControl component

--- src/pages/RemoteControl/RemoteControl.js
+++ src/pages/RemoteControl/RemoteControl.js
@@ -1,76 +1,38 @@
-import { connect } from "react-redux";
-import React, { useState } from "react";
-import "./RemoteControl.css";
-import Tabs from "../../components/Tabs/Tabs";
-// import "react-dropdown/style.css";
-import { Route, useParams } from "react-router-dom";
-import request, { devices } from "../../service";
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
 
-const RemoteControl = (props, { user }) => {
-  const [machines, setMachines] = React.useState();
+import request, { wsEndpoint } from "../../service";
 
-  let { name, tab } = useParams();
+const RemoteControl = ({ machine }) => {
+    const [url, setUrl] = useState('slkadhjaksl');
 
-  const groupId = user?.userGroups[0]?.groupId || 2;
+    const handleClick = () => {
+        request(`${wsEndpoint}/screenshot`, 'POST', { ...machine, user: 'test' })
+            .then(res => {
+                // Wasn't sure what the API is supposed to return so feel free to change what url is set to
+                res && setUrl(res?.message);
+            })
+            .catch(err => console.error(err));
+    };
 
-  if (machines == undefined) {
-    request("https://si-grupa5.herokuapp.com/api/agent/online")
-      .then((res) => {
-        console.log("testee" + JSON.stringify(res));
-        setMachines(res?.data);
-      })
-      .catch((error) => {
-        console.log("Vazan error");
-        console.log(error);
-      });
+    return (
+        <div>
+            <div>
+                <div>
+                    <div className="screenshot">
+                        <p>Screenshot</p>
+                        <img alt="Asked image will appear here." className="screenshot-img" src={`data:image/jpeg;base64,${url}`} />
+                    </div>
+                </div>
+            </div>
 
-    return <div className="page dashboard"></div>;
-  }
-
-  // const switchMachine = (machine) => {
-  //   props.history.push(
-  //     "/remotecontrol/" +
-  //       machine +
-  //       "/" +
-  //       (tab == undefined ? "screenshot" : tab)
-  //   );
-  // };
-
-  //const machineList = [];
-  let machine =
-    name == "0" || name == undefined
-      ? machines.find((value) => value.status !== "Disconnected")
-      : machines.find(
-          (value) => value.name == name && value.status !== "Disconnected"
-        );
-
-  if (machine == undefined) return <div className="page dashboard"></div>;
-
-  // for (const [index, value] of machines.entries()) {
-  //   machineList.push(<option value={value.name}>{value.name}</option>);
-  // }
-  return (
-    <div className="page dashboard">
-      <h1>IWM Remote Access/Control</h1>
-      <br></br>
-      <h2>{name}</h2>
-      <br></br>
-      <div>
-        {/* <select
-          onChange={(event) => switchMachine(event.target.value)}
-          value={name}
-        >
-          {machineList}
-        </select> */}
-        <Tabs history={props.history} machine={machine}></Tabs>
-      </div>
-    </div>
-  );
+            <button type="button" onClick={handleClick}> Get Screenshot </button>
+        </div>
+    );
 };
 
-export default connect(
-  (state) => ({
-    user: state.login.user,
-  }),
-  {}
-)(RemoteControl);
+RemoteControl.propTypes = {
+    machine: PropTypes.object.isRequired
+};
+
+export default RemoteControl;
