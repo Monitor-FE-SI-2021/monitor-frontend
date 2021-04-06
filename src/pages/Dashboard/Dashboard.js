856ffde14f6bbe460a953c92d388a982ecea684e
mehmed <mdziho1@etf.unsa.ba>
2021-04-06 13:16:43 +0200

    dashboard no active machines message

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,9 @@
 import React from "react";
-import {connect} from "react-redux";
+import { connect } from "react-redux";
 import MachineIcon from "../../assets/icons/machine.png";
 import DonutChart from "./components/charts/DonutChart";
 import ActiveMachine from "./components/ActiveMachine";
-import request, {devices} from "../../service";
+import request, { devices } from "../../service";
 import GoogleMapMonitors from "./components/GoogleMapMonitors";
 import "./dashboard.scss";
 
@@ -51,36 +51,37 @@ let hddUsageChart = {
     ],
 };
 
-let activeMachines = [
-    {
-        deviceUid: "fc548ecb-12ec-4ad5-8672-9d5a9565ff60",
-        name: "Desktop PC 1",
-        location: "Sarajevo - BBI",
-        ip: "255.255.255.0",
-        path: "C:/user/programfiles",
-    },
-        {
-        deviceUid: "92649d24-fc46-44c6-b085-356977dbb782",
-        name: "Desktop PC 2",
-        location: "Sarajevo - BBI",
-        ip: "255.255.255.0",
-        path: "C:/user/programfiles",
-    },
-    {
-        deviceUid: "eed19402-67c5-4978-9ad2-513cd5db6376",
-        name: "Desktop PC 1",
-        location: "Sarajevo - SCC",
-        ip: "255.255.255.0",
-        path: "C:/user/programfiles",
-    },
-    {
-        deviceUid: "d3143e54-d497-402f-82ef-b1b213c1d172",
-        name: "Desktop PC 2",
-        location: "Sarajevo - SCC",
-        ip: "255.255.255.0",
-        path: "C:/user/programfiles",
-    }
-];
+// let activeMachines = [
+//     {
+//         deviceUid: "fc548ecb-12ec-4ad5-8672-9d5a9565ff60",
+//         name: "Desktop PC 1",
+//         location: "Sarajevo - BBI",
+//         ip: "255.255.255.0",
+//         path: "C:/user/programfiles",
+//     },
+//         {
+//         deviceUid: "92649d24-fc46-44c6-b085-356977dbb782",
+//         name: "Desktop PC 2",
+//         location: "Sarajevo - BBI",
+//         ip: "255.255.255.0",
+//         path: "C:/user/programfiles",
+//     },
+//     {
+//         deviceUid: "eed19402-67c5-4978-9ad2-513cd5db6376",
+//         name: "Desktop PC 1",
+//         location: "Sarajevo - SCC",
+//         ip: "255.255.255.0",
+//         path: "C:/user/programfiles",
+//     },
+//     {
+//         deviceUid: "d3143e54-d497-402f-82ef-b1b213c1d172",
+//         name: "Desktop PC 2",
+//         location: "Sarajevo - SCC",
+//         ip: "255.255.255.0",
+//         path: "C:/user/programfiles",
+//     }
+// ];
+
 function convertStatistics(statistic) {
     return [Math.round(statistic * 100), Math.round((1 - statistic) * 100)];
 }
@@ -90,15 +91,15 @@ let clickedMachine = null
 let allMachinesUsage = null
 let lastDisconnected = null
 
-const Dashboard = ({user}) => {
-    // let activeMachines = []
+const Dashboard = ({ user }) => {
+    let activeMachines = []
     const [machines, setMachines] = React.useState([]);
     const [active, setActive] = React.useState([...activeMachines]);
     const [showCharts, setShowCharts] = React.useState(false);
 
     function filterActive(activeMachines, allMachines) {
         return activeMachines ? activeMachines.filter((machine) => {
-            const existingMachine = allMachines.find(({deviceUid}) => {
+            const existingMachine = allMachines.find(({ deviceUid }) => {
                 return machine.status !== "Waiting" && machine.deviceUid === deviceUid;
             });
             if (existingMachine) {
@@ -118,47 +119,48 @@ const Dashboard = ({user}) => {
             })
             .catch((err) => console.log(err));
     }
-/*
+
+    /*
+        React.useEffect(() => {
+            request(devices + "/AllDevices")
+                .then((res) => {
+                    const allMachines = res.data.data;
+                    setMachines(allMachines);
+                    console.log(allMachines)
+                    request("https://si-grupa5.herokuapp.com/api/agent/online")
+                        .then((res) => {
+                            console.log(res)
+                            setActive(filterActive(res?.data, allMachines));
+                        })
+                })
+                .catch((err) => console.log(err));
+
+            request(devices + "/GetAverageHardwareUsageForUser")
+                .then((res) => {
+                    allMachinesUsage = res.data.data
+                    console.log(allMachinesUsage)
+                    setCharts(allMachinesUsage, { name: "All machines"})
+                })
+        }, []);
+    */
+
     React.useEffect(() => {
         request(devices + "/AllDevices")
             .then((res) => {
                 const allMachines = res.data.data;
                 setMachines(allMachines);
-                console.log(allMachines)
                 request("https://si-grupa5.herokuapp.com/api/agent/online")
                     .then((res) => {
-                        console.log(res)
                         setActive(filterActive(res?.data, allMachines));
                     })
+                // setActive(filterActive(activeMachines, allMachines))
             })
             .catch((err) => console.log(err));
 
         request(devices + "/GetAverageHardwareUsageForUser")
             .then((res) => {
                 allMachinesUsage = res.data.data
-                console.log(allMachinesUsage)
-                setCharts(allMachinesUsage, { name: "All machines"})
-            })
-    }, []);
-*/
-
-    React.useEffect(() => {
-        request(devices + "/AllDevices")
-            .then((res) => {
-                const allMachines = res.data.data;
-                setMachines(allMachines);
-                // request("https://si-grupa5.herokuapp.com/api/agent/online")
-                //     .then((res) => {
-                //         setActive(filterActive(res?.data, allMachines));
-                //     })
-               setActive(filterActive(activeMachines, allMachines))
-            })
-            .catch((err) => console.log(err));
-
-        request(devices + "/GetAverageHardwareUsageForUser")
-            .then((res) => {
-                allMachinesUsage = res.data.data
-                setCharts(allMachinesUsage, { name: "All machines"})
+                setCharts(allMachinesUsage, { name: "All machines" })
             })
 
     }, []);
@@ -180,7 +182,7 @@ const Dashboard = ({user}) => {
             cloned.splice(index, 1);
             lastDisconnected = machine
             if (cloned.length === 0 || removedMachine?.deviceId === clickedMachine?.deviceId) {
-                clickedMachine = {name: "All machines"}
+                clickedMachine = { name: "All machines" }
                 setCharts(allMachinesUsage, clickedMachine)
             }
             setActive(cloned);
@@ -212,23 +214,29 @@ const Dashboard = ({user}) => {
                 <div className="row machine-cards">
                     <h1>List of active machines</h1>
                     <div className="scrollable">
-                        {active.map((machine, id) => (
-                            <ActiveMachine
-                                key={id}
-                                data={machine}
-                                img={MachineIcon}
-                                onDisconnect={disconnectMachine}
-                                getStatistics={getStatistics}
-                            />
-                        ))}
+                        {active?.length ? (
+                            active.map((machine, id) => (
+                                <ActiveMachine
+                                    key={id}
+                                    data={machine}
+                                    img={MachineIcon}
+                                    onDisconnect={disconnectMachine}
+                                    getStatistics={getStatistics}
+                                />
+                            ))
+                        ) : (
+                            <div className='no-active-machines'>
+                                No active machines.
+                            </div>
+                        )}
                     </div>
                 </div>
-                
+
                 {showCharts && (
                     <div>
                         <h2 className="machineName">{clickedMachine?.name}</h2>
                         <div className="chartContainer">
-                            
+
                             <div className="row">
                                 <DonutChart
                                     displayTitle="Average RAM usage"
@@ -249,7 +257,7 @@ const Dashboard = ({user}) => {
                                     chartData={hddUsageChart}
                                 />
                             </div>
-                            
+
                         </div>
                     </div>
                 )}
@@ -258,9 +266,9 @@ const Dashboard = ({user}) => {
                 <GoogleMapMonitors
                     activeMachines={active}
                     allMachines={machines}
-                />  
+                />
             </div>
-            
+
         </div>
     );
 };
