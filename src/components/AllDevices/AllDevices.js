86140c7965add6a20ca9f98b0cf60fe211571fe0
abecirovic3 <abecirovic3@etf.unsa.ba>
2021-04-09 16:24:19 +0200

    done initial AllDevices component

--- src/components/AllDevices/AllDevices.js
+++ src/components/AllDevices/AllDevices.js
@@ -1,23 +1,27 @@
 import DeviceTable from "../DeviceTable/DeviceTable";
-import { useEffect } from "react"
-import {fetchAllDevices} from "../../store/modules/devices/actions";
+import { useEffect, useState } from "react"
 import { connect } from "react-redux";
+import request, {devices} from "../../service";
 
-const AllDevices = ({ allDevices, devicesSearchText }) => {
+const AllDevices = ({ devicesSearchText }) => {
+
+    const [searchedDevices, setSearchedDevices] = useState([{}])
 
     useEffect(() => {
-        fetchAllDevices()
-    }, [fetchAllDevices])
+        request(devices + `/AllDevices?name=${devicesSearchText}&location=${devicesSearchText}`)
+            .then(response => response.data)
+            .then(r => {
+                setSearchedDevices(r.data)
+            })
+    }, [devicesSearchText])
 
     return (
         <div>
-            <button onClick={console.log(devicesSearchText)}>OK</button>
-            <button onClick={console.log(allDevices)}>OK</button>
+            <DeviceTable devices={searchedDevices} />
         </div>
     )
 }
 
 export default connect((state) => ({
-    devicesSearchText: state.devices.searchText,
-    allDevices: state.devices.allDevices
-}),{ fetchAllDevices })(AllDevices)
+    devicesSearchText: state.devices.searchText
+}),{})(AllDevices)
