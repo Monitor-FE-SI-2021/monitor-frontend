ee8df6a592caeea397619e5cd61ceb7c0283dddf
abecirovic3 <abecirovic3@email.com>
2021-04-14 12:08:49 +0200

    Basic layout for Admin Panel

--- src/pages/AdminPanel/AdminPanel.js
+++ src/pages/AdminPanel/AdminPanel.js
@@ -1,9 +1,23 @@
-const AdminPanel = () => {
+import {RouteLink} from "../../store/modules/menu/menu";
+import { connect } from "react-redux";
+import React from "react";
+import { push } from "connected-react-router";
+import "./AdminPanel.scss"
+
+const AdminPanel = ({ push }) => {
     return (
-        <div>
-            <h3>Admin Panel</h3>
+        <div className="page admin-panel">
+            <div className="top">
+                <span className='page-title'> Admin Panel </span>
+                <div>
+                    <button className="custom-btn" onClick={() => push(RouteLink.ManageDevice)}>Kreiraj korisnika</button>
+                </div>
+            </div>
+            <div>
+                <h3>Tabela svih korisnika ?</h3>
+            </div>
         </div>
     )
 }
 
-export default AdminPanel
+export default connect(() => {}, { push })(AdminPanel)
