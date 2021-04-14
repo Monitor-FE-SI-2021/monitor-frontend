a2ec1ee645fb84a12217fe0126afc5077e4417c5
abecirovic3 <abecirovic3@email.com>
2021-04-14 13:38:30 +0200

    Basic layout for Manage User Form

--- src/components/ManageUserForm/ManageUserForm.js
+++ src/components/ManageUserForm/ManageUserForm.js
@@ -1,9 +1,43 @@
-const MangeUserForm = () => {
+import React from "react";
+import { connect } from "react-redux";
+import { TextField, MenuItem } from '@material-ui/core';
+import { showSwalToast } from "../../utils/utils";
+import { RouteLink } from "../../store/modules/menu/menu";
+import { push } from "connected-react-router";
+import {AsyncButton} from "../AsyncButton/AsyncButton";
+import "../ManageDeviceForm/ManageDeviceForm.scss"
+
+const MangeUserForm = ({ push }) => {
     return (
-        <div>
-            <h3>Users Form</h3>
-        </div>
-    )
+        <form className="manage-device-form">
+            <TextField variant="outlined" label="Ime" name="name" />
+
+            <TextField variant="outlined" label="Prezime" name="lastname" />
+
+            <TextField variant="outlined" label="Email" name="email" />
+
+            <TextField variant="outlined" label="Broj telefona" name="phone" />
+
+            <TextField variant="outlined" label="Šifra" type="password" name="password" />
+
+            <TextField variant="outlined" label="Ponovite šifru" type="password" name="passwordRepeat" />
+
+            <TextField
+                variant="outlined"
+                select
+                name="roleId"
+                label="Uloga"
+                className="group-selector"
+            />
+
+            <div className='buttons'>
+                <button className="custom-btn outlined" onClick={() => push(RouteLink.AdminPanel)}>Otkaži</button>
+                <AsyncButton className="custom-btn" >
+                    Kreiraj korisnika
+                </AsyncButton>
+            </div>
+        </form>
+    );
 }
 
-export default MangeUserForm
+export default connect(() => {}, { push })(MangeUserForm)
