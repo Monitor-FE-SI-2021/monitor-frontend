fcea24ca432080ea1fbd802f53806169d96ad79f
Ajdin <ajdinh1997@gmail.com>
2021-05-03 18:28:56 +0100

    Added tests for Dashboard

--- src/tests/pages/Dashboard/Dashboard.spec.js
+++ src/tests/pages/Dashboard/Dashboard.spec.js
@@ -1,4 +1,7 @@
-import { convertStatistics, machineNameAndLocation } from "../../../pages/Dashboard/Dashboard";
+import { convertStatistics, machineNameAndLocation, filterActive} from "../../../pages/Dashboard/Dashboard";
+import Dashboard from "../../../pages/Dashboard/Dashboard";
+import React from "react";
+import ReactDOM from 'react-dom';
 
 describe('Dashboard tests', () => {
     it('Correct statistic calculated', () => {
@@ -20,5 +23,22 @@ describe('Dashboard tests', () => {
     it('Machine name with location', () => {
         const machine={name: "Machine1", location: "Sarajevo"};
         expect(machineNameAndLocation(machine)).toStrictEqual("Machine1 (Sarajevo)");
+    })
+
+    it('Active machines successfully filtered', () => {
+        const allMachines=[{"deviceUid":"1","lastTimeOnline":"3.3.2021","deviceId":"1"},{"deviceUid":"2","lastTimeOnline":"4.3.2021","deviceId":"2"}];
+        let activeMachines=[{"deviceUid":"1","status":"in use","user":"Aj"},{"deviceUid":"3","status":"in use","user":"Tj"}];
+        const user={"email":"osoba5@email.com"};
+        const setActiveForUser=(machines)=>{console.log(machines)};
+        activeMachines=filterActive(activeMachines,allMachines,setActiveForUser,user);
+
+        expect(activeMachines.length).toStrictEqual(1);
+    })
+
+    it('Dashboard renders successfully', () => {
+        const div =document.createElement("div");
+        const user={"email":"osoba5@email.com"};
+       
+        ReactDOM.render(<Dashboard user={user} ></Dashboard>, div)
     })
 })
