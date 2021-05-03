88e17034d025e642c667935d52d65d91738d9392
akantic1 <akantic1@etf.unsa.ba>
2021-05-03 18:42:10 +0200

    testovi

---
+++ src/tests/components/ReportTable/ReportTable.spec.js
@@ -0,0 +1,15 @@
+// import store from "../../../store/store"
+// import ReportTable from "../../../components/ReportTable/ReportTable";
+// import { render, screen } from "@testing-library/react";
+// import {Provider} from "react-redux";
+
+// // describe("Test ", () => {
+// //     it("Should have name", () => {
+// //         render(<Provider store={store}>
+// //                 <ReportTable />
+// //             </Provider>)
+// //         screen.getByText('Data');
+// //     });
+    
+
+// // })
---
+++ src/tests/pages/Report/ReportList.spec.js
@@ -0,0 +1,42 @@
+import store from "../../../store/store"
+import ReportList from "../../../pages/Reporting/ReportList";
+import { render, screen } from "@testing-library/react";
+import {Provider} from "react-redux";
+
+describe("Test report table page", () => {
+    it("Page name", () => {
+        render(
+            <Provider store={store}>
+                <ReportList />
+            </Provider>)
+        screen.getByText('Report List');
+    });
+
+    it("Page filter", () => {
+        render(
+            <Provider store={store}>
+                <ReportList />
+            </Provider>)
+        screen.getByText('Filter');
+    });
+
+    it("Page reset filter", () => {
+        render(
+            <Provider store={store}>
+                <ReportList />
+            </Provider>)
+        screen.getByText('Reset filters');
+    });
+
+    it("New report button", () => {
+        render(
+            <Provider store={store}>
+                <ReportList />
+            </Provider>)
+        screen.getByText('New report');
+    });
+
+  
+   
+});
+
