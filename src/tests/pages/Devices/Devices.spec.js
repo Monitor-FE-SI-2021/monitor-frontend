63fd04c20e2e74810cbf6ebc4570930a80f19233
TMehulic <tarikmehulic@hotmail.com>
2021-05-03 12:47:26 +0200

    Added few tests

---
+++ src/tests/pages/Devices/Devices.spec.js
@@ -0,0 +1,48 @@
+import {render, screen} from "@testing-library/react";
+import {Provider} from "react-redux";
+import store from "../../../store/store";
+import Devices from "../../../pages/Devices/Devices";
+
+
+describe("Test if devices is rendered properly", () => {
+    it("Should have pregled mašina", () => {
+        render(
+            <Provider store={store}>
+                <Devices />
+            </Provider>
+        )
+        screen.getByText('Pregled mašina');
+    });
+    it("Should have kreiraj grupu", () => {
+        render(
+            <Provider store={store}>
+                <Devices />
+            </Provider>
+        )
+        screen.getByText('Kreiraj grupu');
+    });
+    it("Should have kreiraj mašinu", () => {
+        render(
+            <Provider store={store}>
+                <Devices />
+            </Provider>
+        )
+        screen.getByText('Kreiraj mašinu');
+    });
+    it("Should have pretraži grupe", () => {
+        render(
+            <Provider store={store}>
+                <Devices />
+            </Provider>
+        )
+        screen.getAllByText('Pretraži grupe');
+    });
+    it("Should have pretraži uređaje", () => {
+        render(
+            <Provider store={store}>
+                <Devices />
+            </Provider>
+        )
+        screen.getAllByText('Pretraži uređaje');
+    });
+});
