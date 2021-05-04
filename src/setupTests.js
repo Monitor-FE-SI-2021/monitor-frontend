9a9048a264e8de4336e8ec30f00a143dbbf03521
mehmed <mdziho1@etf.unsa.ba>
2021-05-04 19:16:54 +0200

    mock localStorage

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,26 @@ import { initialize } from "@googlemaps/jest-mocks";
 beforeEach(() => {
     initialize();
 });
+
+class LocalStorageMock {
+    constructor() {
+        this.store = {};
+    }
+    clear() {
+        this.store = {};
+    }
+    getItem(key) {
+        return this.store[key] || null;
+    }
+    setItem(key, value) {
+        this.store[key] = value.toString();
+    }
+    removeItem(key) {
+        delete this.store[key];
+    }
+}
+
+const localStorage = new LocalStorageMock();
+
+// jest test environment expects these to exist
+window.localStorage = localStorage
