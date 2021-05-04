c477c08ce3cefccf114a187dd6edbcee08ec2c3d
mehmed <mdziho1@etf.unsa.ba>
2021-05-04 19:30:06 +0200

    jest maxWorkers

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,28 +1,36 @@
 import { initialize } from "@googlemaps/jest-mocks";
 
-beforeEach(() => {
-    initialize();
-});
-
 class LocalStorageMock {
     constructor() {
         this.store = {};
     }
+
     clear() {
         this.store = {};
     }
+
     getItem(key) {
         return this.store[key] || null;
     }
+
     setItem(key, value) {
-        this.store[key] = value.toString();
+        this.store[key] = String(value);
     }
+
     removeItem(key) {
         delete this.store[key];
     }
-}
+};
 
-const localStorage = new LocalStorageMock();
+let storage;
 
-// jest test environment expects these to exist
-window.localStorage = localStorage
+beforeEach(() => {
+    initialize();
+    storage = window.localStorage;
+    window.localStorage = new LocalStorageMock();
+});
+
+afterEach(() => {
+    window.localStorage = storage
+})
+
