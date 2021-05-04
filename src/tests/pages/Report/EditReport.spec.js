4c6bac4f64fba2bd433f8816539ccf86be1ca379
Merjem Bajraktarevic <meribajraktarevic@gmail.com>
2021-05-04 18:45:27 +0200

    more potential tests

--- src/tests/pages/Report/EditReport.spec.js
+++ src/tests/pages/Report/EditReport.spec.js
@@ -19,4 +19,68 @@ describe("Test edit report page", () => {
           </Provider>)
       screen.getByText('Loading...');
     });
+
+    // it("Report title ", async () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('Report title:');
+    // });
+
+    // it("Send email ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('Do you want to be send an email with this report?');
+    // });
+
+    // it("Current Frequency: ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('Current Frequency:');
+    // });
+
+    // it("Current Time: ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('Current Time:');
+    // });
+
+    // it("Edit time ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('CHANGE TIME');
+    // });
+
+    // it("Edit query ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('CHANGE GROUP');
+    // });
+
+    // it("Show query ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('Query:');
+    // });
+
+    // it("Submit button ", () => {
+    //   render(
+    //       <Provider store={store}>
+    //           <EditReport />
+    //       </Provider>)
+    //   screen.getByText('Submit');
+    // });
 });
